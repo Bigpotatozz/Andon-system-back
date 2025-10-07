fc0714f1ef84d4a3ee58aaa047973b046163d581
oscar <oscarmancilla227@gmail.com>
2025-10-07 10:52:41 -0600

    correcciones

--- Config/connection.js
+++ Config/connection.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "1924",
-  database: "act2",
+  password: "",
+  database: "",
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
