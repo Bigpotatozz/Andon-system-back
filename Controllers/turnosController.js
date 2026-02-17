ce7cb17178128cb6db6011d138e91ff19a5a6e9d
oscar <oscarmancilla227@gmail.com>
2026-02-17 14:12:12 -0600

    add: comentarios turnosController desarrollo

--- Controllers/turnosController.js
+++ Controllers/turnosController.js
@@ -1,28 +1,31 @@
 const { pool } = require("../Config/connection");
 
+//Endpoint para agregar un turno nuevo (VERSION ALTERNATIVA, NO LISTA PARA PRODUCCION)
 const agregarTurnos = async (req, res) => {
+  //Accede al objeto de turnos
   const { turnos } = req.body;
+  //Accede al id de la linea de produccion
   const { idLineaProduccion } = req.params;
   try {
+    //Recorre cada uno de los turnos del body
     for (let turno of turnos) {
+      //Va insertando el turno
       const query = `INSERT INTO turnos (nombreTurno, horaInicio, horaFin, cicleTime) VALUES (?,?,?,?)`;
-
       const values = [
         turno.nombreTurno,
         turno.horaInicio,
         turno.horaFin,
         turno.cicleTime,
       ];
-
       const turnosAgregados = await pool.query(query, values);
 
+      //Query que inserta en el detalleTurno
       const query2 = `INSERT INTO detalleTurno (idTurno, idLineaProduccion) VALUES (?, ?)`;
-
       const values2 = [turnosAgregados.insertId, idLineaProduccion];
-
       await pool.query(query2, values2);
     }
 
+    //Devuelve la respuesta exitosa
     res.status(200).json({ message: "Turnos agregados correctamente" });
   } catch (error) {
     console.log(error);
