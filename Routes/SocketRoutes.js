4d13853b4b2a0e4bafc21f0e86924ed1ef5012f6
oscar <oscarmancilla227@gmail.com>
2026-02-17 14:41:20 -0600

    add: comment socketRoutes

--- Routes/SocketRoutes.js
+++ Routes/SocketRoutes.js
@@ -5,18 +5,23 @@ const {
 } = require("../Controllers/lineasController");
 const { socketObtenerTurno } = require("../Controllers/productionController");
 
+//Seccion de sockets
 const socketRoutes = async (io) => {
+  //Se incia el socket y se definen las "rutas"
   io.on("connection", (socket) => {
     console.log("Cliente conectado");
 
+    //Socket que obtiene los estatus
     socket.on("obtenerEstatus", () => {
       socketObtenerEstatus(socket);
     });
 
+    //socket que obtiene los turnos
     socket.on("obtenerTurno", () => {
       socketObtenerTurno(socket);
     });
 
+    //socket que obtiene las estaciones
     socket.on("obtenerEstaciones", () => {
       socketObtenerEstaciones(socket);
     });
