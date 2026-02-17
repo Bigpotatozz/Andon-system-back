490113f91e9bda1f91cf6cfceeaa72a9182250e3
oscar <oscarmancilla227@gmail.com>
2026-02-17 14:09:41 -0600

    add: comentarios productionController

--- Controllers/productionController.js
+++ Controllers/productionController.js
@@ -1,23 +1,27 @@
 const { pool } = require("../Config/connection");
 
+//Envia la informacion para la pantalla production ratio
 const obtenerProductionRatio = async (req, res) => {
+  //Se accede al turno indicado en el body
   const { idTurno } = req.params;
   try {
+    //Accede a la informacion y hace los joins de la linea con su respectivo turno
     const productionRatioQuery = `select * from lineaproduccion 
                                 join turno on turno.idLineaProduccion = lineaproduccion.idLineaProduccion
                                 join objetivo on objetivo.idTurno = turno.idTurno
                                 where turno.idTurno = ?
                                 LIMIT 1                                
                                 `;
-
     const productionRatio = await pool.query(productionRatioQuery, [idTurno]);
 
+    //Si no hay nada del resultado envia el error
     if (!productionRatio) {
       return res.status(404).send({
         message: "No hay turnos registrados",
       });
     }
 
+    //Envia la informacion obtenida
     return res.status(200).send({
       productionRatio: productionRatio[0],
     });
@@ -28,8 +32,10 @@ const obtenerProductionRatio = async (req, res) => {
   }
 };
 
+//Actualizar el progreso de produccion (+1)
 const actualizarProgresoProduccion = async (req, res) => {
   try {
+    //Se accede al turno en base a la hora
     const queryTurno = `SELECT * 
                                 FROM turno
                                 WHERE (
@@ -38,14 +44,15 @@ const actualizarProgresoProduccion = async (req, res) => {
                                   (horaInicio > horaFin AND (CURTIME() >= horaInicio OR CURTIME() < horaFin))
                                 )
                                 LIMIT 1`;
-
     const turno = await pool.query(queryTurno);
     console.log(turno[0][0]);
 
+    //Una vez teniendo el turno se aumenta 1 al progreso de produccion correspondiente al turno
     const query =
       "update objetivo set progresoProduccion = progresoProduccion + 1, progresoProduccionHora = progresoProduccionHora + 1 where idTurno = ?";
     const response = await pool.query(query, [turno[0][0].idTurno]);
 
+    //Devuelve un estatus exitoso
     return res.status(200).send({
       message: "Progreso actualizado",
     });
@@ -56,8 +63,10 @@ const actualizarProgresoProduccion = async (req, res) => {
   }
 };
 
+//Obtiene el turno en base a la hora
 const obtenerTurno = async (req, res) => {
   try {
+    //Query para obtener el turno
     const queryEnviarTurno = `
                                 SELECT * 
                                 FROM turno
@@ -72,6 +81,7 @@ const obtenerTurno = async (req, res) => {
 
     console.log(response);
 
+    //Devuelve la respuesta exitosa
     return res.status(200).send({
       turno: response[0],
     });
@@ -82,13 +92,16 @@ const obtenerTurno = async (req, res) => {
   }
 };
 
+//Inicia en 0 el progreso de produccion por hora
 const resetearProgresoProduccionHora = async (req, res) => {
+  //Accede al turno
   const { turno } = req.params;
   try {
+    //Query para resetear el progreso de produccion por hora
     const query = `update objetivo set progresoProduccionHora = 0 where idTurno = ?`;
-
     const reseteo = await pool.query(query, [turno]);
 
+    //Devuelve un estatus exitoso
     return res.status(200).send({
       message: "Progreso reseteado",
     });
@@ -99,11 +112,14 @@ const resetearProgresoProduccionHora = async (req, res) => {
   }
 };
 
+//Obtiene los turnos registrados
 const obtenerTurnos = async (req, res) => {
   try {
+    //Obtiene los turnos
     const query = `SELECT * FROM turno`;
     const turnos = await pool.query(query);
 
+    //Devuelve una respuesta exitosa
     return res.status(200).send({
       turnos: turnos[0],
     });
@@ -116,7 +132,10 @@ const obtenerTurnos = async (req, res) => {
   }
 };
 
+//Socket para obtener los turnos en caso de cambio (en base a la hora)
 const socketObtenerTurno = async (socket) => {
+  //Query que obtiene el turno en base a la hora actual
+  //En caso de detectar el cambio emite el evento al cliente
   const socketQuery = `
                                 SELECT * 
                                 FROM turno
@@ -131,8 +150,10 @@ const socketObtenerTurno = async (socket) => {
 
   const estatusInterval = setInterval(async () => {
     try {
+      //Ejecuta la query
       const response = await pool.query(socketQuery);
 
+      //Emite el cambio al cliente
       socket.emit("obtenerTurno", response[0]);
     } catch (e) {
       console.log(e);
